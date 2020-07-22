5619e9e9401debf2f80016d8c08ce435fea4ce05
Eddie Jones <eddiejones2108@gmail.com>
2020-07-22 15:18:44 +0100

    more style updates

--- script.js
+++ script.js
@@ -1,73 +1,18 @@
-'use strict';
 
-let active = { x: 1, y: 0 }
-const slides = document.getElementsByTagName("section");
-const posts = document.getElementsByTagName("article")
+function updateView(event) {
+  for (let sec of document.getElementsByTagName("section")) {
+    let rec = sec.getBoundingClientRect();
 
-function gotoView(x, y) {
-  active = {x : x, y: y};
-  updateView();
-}
+    // Height of the section not in the window
+    let off = (Math.max(rec.bottom - window.innerHeight, 0) - Math.min(rec.top, 0)) / (1.5 * rec.height);
+    console.log(off, rec.height);
 
-function updateView() {
-  const r = 5;
-  const h_delta = 10;
-  const v_detla = 100 + (r * h_delta);
-  const y = -v_detla * (active.y > 0 ? active.y + (r / 2) : 0);
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = "25%";
-    switch (i - active.x) {
-      case -2:
-        slides[i].style.transform = "translate(" + String(-2 * (h_delta + 100)) + "%, " + String(y / r) + "%)";
-        break;
-      case -1:
-        slides[i].style.transform = "translate(" + String(-1 * (h_delta + 100)) + "%, " + String(y / r) + "%)";
-        break;
-      case 0:
-        slides[i].style.transform = "translate(0, " + String(y / r) + "%)";
-        if (active.y == 0) {
-          slides[i].style.opacity = "100%";
-        }
-        break;
-      case 1:
-        slides[i].style.transform = "translate(" + String(1 * (h_delta + 100)) + "%, " + String(y / r) + "%)";
-        break;
-      case 2:
-        slides[i].style.transform = "translate(" + String(2 * (h_delta + 100)) + "%, " + String(y / r) + "%)";
-        break;
+    if ((rec.top <= 0 && rec.bottom >= window.innerHeight) || off < 0.25) {
+      // The section is greater than the window size or 75% of box is showing
+      sec.style.opacity = 1;
     }
-  }
-
-  for (let j = 0; j < posts.length; j++) {
-    let offset = v_detla * j
-    posts[j].style.transform = "translate(" + String((1 - active.x) * (h_delta + 100)) + "%," + String(y + offset) + "%)";
-    posts[j].style.opacity = "25%";
-    if (j == active.y - 1) {
-      posts[j].style.opacity = "1";
+    else {
+      sec.style.opacity = Math.pow(1 - off, 2);
     }
   }
 }
-
-document.addEventListener("keydown", function (event) {
-  if (event.keyCode == 13 && active.y >= 1) {
-    window.location.href = posts[active.y - 1].getAttribute("href");
-    event.preventDefault();
-  }
-  else if (event.keyCode == 37 && active.y == 0 && active.x >= 1) {
-    active.x--;
-    event.preventDefault();
-    updateView();
-  } else if (event.keyCode == 38 && active.x == 1 && active.y >= 1) {
-    active.y--
-    event.preventDefault();
-    updateView();
-  } else if (event.keyCode == 39 && active.y == 0 && active.x <= 1) {
-    active.x++
-    event.preventDefault();
-    updateView();
-  } else if (event.keyCode == 40 && active.x == 1 && active.y < posts.length) {
-    active.y++
-    event.preventDefault();
-    updateView();
-  }
-})
