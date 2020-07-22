cb1a4d2d6126999963b1f9c02dd6ef023c974417
Eddie Jones <eddiejones2108@gmail.com>
2020-07-22 17:46:52 +0100

    UI changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function updateView(event) {
     let off = (Math.max(rec.bottom - window.innerHeight, 0) - Math.min(rec.top, 0)) / (1.5 * rec.height);
     console.log(off, rec.height);
 
-    if ((rec.top <= 0 && rec.bottom >= window.innerHeight) || off < 0.25) {
+    if ((rec.top <= 50 && rec.bottom >= window.innerHeight) || off < 0.25) {
       // The section is greater than the window size or 75% of box is showing
       sec.style.opacity = 1;
     }
